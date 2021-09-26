97a4d771edf3986fdddaad90a0a72c6b7fb0c4ff
malinvaudpaul <paulmalinvaud@protonmail.com>
2021-09-26 21:00:14 +0200

    Tâche de génération de d'image de puml avec plugin gulp + wip plugin coveralls

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
 const gulp = require("gulp");
 const asciidoctor = require("@asciidoctor/gulp-asciidoctor");
+const coveralls = require("gulp-coveralls");
+const puml = require("gulp-puml");
 
 function processAdocFiles(cb) {
-  gulp.src("/doc/*.adoc").pipe(asciidoctor({})).pipe(gulp.dest("/doc"));
+  gulp.src("/doc/*.adoc").pipe(asciidoctor({})).pipe(gulp.dest("doc/"));
   cb();
 }
 
 function copyImages(cb) {
-  gulp.src("/doc/*.jpg").pipe(gulp.dest("/doc"));
+  gulp.src("/doc/*.jpg").pipe(gulp.dest("doc/"));
   cb();
 }
 
-exports.default = gulp.parallel(processAdocFiles, copyImages);
+function coverAlls(cb) {
+  gulp.src("test/coverage/**/lcov.info").pipe(coveralls());
+  cb();
+}
+
+function generatePuml(cb) {
+  gulp.src("doc/*.puml").pipe(puml()).pipe(gulp.dest("doc/"));
+  cb();
+}
+
+exports.default = gulp.parallel(processAdocFiles, copyImages, generatePuml);
