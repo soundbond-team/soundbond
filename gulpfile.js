bc0d04ac6e967a72b0c0486125a18f7addb9c738
malinvaudpaul <paulmalinvaud@protonmail.com>
2021-10-05 14:17:31 +0200

    Nouvelle structure pour docs

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,35 @@
 const gulp = require("gulp"),
   asciidoctor = require("@asciidoctor/gulp-asciidoctor"),
-  html2pdf = require("gulp-html2pdf");
+  html2pdf = require("gulp-html2pdf"),
+  clean = require("gulp-clean");
 
 function processAdocFiles(cb) {
-  gulp.src("doc/*.adoc").pipe(asciidoctor({})).pipe(gulp.dest("doc/"));
+  gulp
+    .src("docs/asciidoc/*.adoc")
+    .pipe(asciidoctor({}))
+    .pipe(gulp.dest("docs/asciidoc/generated-docs/"));
   cb();
 }
 
 function copyImages(cb) {
-  gulp.src("doc/*.html").pipe(gulp.dest("doc/"));
+  gulp
+    .src("docs/asciidoc/*.adoc")
+    .pipe(gulp.dest("docs/asciidoc/generated-docs/"));
   cb();
 }
 
 function htmlToPDF(cb) {
-  gulp.src("doc/sb.html").pipe(html2pdf()).pipe(gulp.dest("doc/"));
+  gulp
+    .src("docs/asciidoc/generated-docs/*.html")
+    .pipe(html2pdf())
+    .pipe(gulp.dest("docs/asciidoc/generated-docs/"));
   cb();
 }
+
+gulp.task("clean-up-docs", function () {
+  gulp.src("docs/asciidoc/generated-docs", { read: false }).pipe(clean());
+});
+
+//gulp.task("test", shell.task("npm test", { ignoreErrors: true }));
 
 exports.asciidoc = gulp.parallel(processAdocFiles, copyImages, htmlToPDF);
