c15e4c9f378a7f0ae644eb1616c70c4b8d584d70
Maguitadiop <diopma221@gmail.com>
2021-10-07 21:30:50 +0200

    navbar

--- client/src/components/layouts/Header.js
+++ client/src/components/layouts/Header.js
@@ -1,41 +1,45 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
 
-import React from 'react';
-// We import bootstrap to make our application look better.
-import "bootstrap/dist/css/bootstrap.css";
- 
-// We import NavLink to utilize the react router.
-import { NavLink,form } from "react-router-dom";
 
-const Header = () => {
-    return(
-        <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <NavLink to="/" className="navbar-brand">SoundBond</NavLink>
-          <div className="collpase navbar-collapse">
-            <ul className="navbar-nav mr-auto">
-              <li className="navbar-item">
-                <NavLink to="/accueil" className="nav-link">Accueil</NavLink>
-              </li>
-              <li className="navbar-item">
-                <NavLink to="/activite" className="nav-link">Activité</NavLink>
-              </li>
-              <li>
-                <form class="d-flex">
-                  <input class="form-control me-2" type="search" placeholder="Chercher" aria-label="Chercher"/>
-                  <button class="btn btn-outline-success" type="submit">Chercher</button>
-                </form>
-              </li>
-            </ul>
-            
-            <ul className="navbar-nav">
-              <li className="navbar-item">
-                <NavLink to="/" className="nav-link">Profil</NavLink>
-              </li>
-              </ul>
-          </div>
-        </nav>
-    </div>
+  const Navbar = () =>{
+    return (
+      <>
+     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">SoundBond</a>
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+       </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Accueil<span class="sr-only"></span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">A propos</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Profil
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+          
+        </ul>
+      </div>
+</nav>
+      </>
     )
-}
+  }
 
-export default Header;
+
+export default Navbar;
---
+++ client/src/components/layouts/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () =>{
+    return(
+        <div>
+            <p>WELCOME</p>
+            <h1>Hello word</h1>
+        </div>
+    )
+}
+
+export default Home
