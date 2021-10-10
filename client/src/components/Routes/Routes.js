263514be85d167e12a5d9cb2ab97c5567f6386ac
Mohamed Belhaimeur <belhaim789@gmail.com>
2021-10-10 20:09:05 +0200

    coquille

--- client/src/components/Routes/Routes.js
+++ client/src/components/Routes/Routes.js
@@ -5,7 +5,7 @@ import {
   Route,
   Switch,
 } from "react-router-dom";
-import home from "../../pages/home";
+import home from "../../Pages/home";
 //import MapPage from "../../pages/MapPage";
 import Nav from "../../components/Navbar/Navbar";
 //le routing ne sert a rien tant quil nya pas une persistance pour tt les elements ( post )
