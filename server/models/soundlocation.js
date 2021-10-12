40c902237f399425181b66994717fde7efd529f8
malinvaudpaul <paulmalinvaud@protonmail.com>
2021-10-12 23:08:51 +0200

    Diagramme de classe v0.3

--- server/models/soundlocation.js
+++ server/models/soundlocation.js
@@ -4,11 +4,11 @@ la latitude et la longitude d'un son.  */
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class SoundLocation extends Model { }
+  class SoundLocation extends Model {}
   SoundLocation.init(
     {
-      longitude:    DataTypes.DECIMAL(9, 6),
-      latitude:     DataTypes.DECIMAL(8, 6),
+      longitude: DataTypes.DECIMAL(9, 6),
+      latitude: DataTypes.DECIMAL(8, 6),
       // clé étrangère vers Sound.
     },
     {
