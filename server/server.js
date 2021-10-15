9e014828bed5df29830d54c654064b956400a50c
gu1lhem <38001093@parisnanterre.fr>
2021-10-15 14:46:34 +0200

    Comments [ci skip]

--- server/server.js
+++ server/server.js
@@ -11,24 +11,16 @@ const port = process.env.PORT || 8080; // Port du serveur de développement.
 app.use(cors());
 app.use(express.json());
 
+// On dit au serveur de servir ces pages.
 require("./routes/sound.routes")(app);
 require("./routes/soundlocation.routes")(app);
 require("./routes/post.routes")(app);
-
-// On dit au serveur de servir ces pages.
-//app.use("/api/sound", exercisesRouter);
-// Il n'est peut-être pas nécessaire d'utiliser cette ligne car
-// // Instanciation des fichiers de route.
-//const exercisesRouter = require("./routes/sound");
-// a été remplacé par
-// require("./routes/sound.routes")(app);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
 
 // Récupération de la base de données
-
 const db = require("./models");
 
 // Test de la connexion.
@@ -40,11 +32,5 @@ try {
 }
 
 db.sequelize.sync();
-
-/*
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and re-sync db.");
-});
-*/
 
 module.exports = app;
