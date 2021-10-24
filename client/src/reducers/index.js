64decb8024fad5c2b117f2fef8a3da41490bf61f
gu1lhem <sky@anche.no>
2021-10-24 17:26:41 +0200

    Refactoring posts reducer.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 
 import soundlocationReducer from "./soundlocation.reducer";
 import soundReducer from "./sound.reducer";
-import postReducer from "./posts.reducer";
+import postReducer from "./post.reducer";
 import onesoundlocationReducer from "./onesoundlocation.reducer";
 
 export default combineReducers({
