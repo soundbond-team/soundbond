2e3922da8e33715e6328307ac23abec2d5afc938
Maguitadiop <diopma221@gmail.com>
2021-10-25 22:35:54 +0200

    partager sans url de l'audio

--- client/src/components/Routes/Routes.js
+++ client/src/components/Routes/Routes.js
@@ -6,7 +6,7 @@ import {
   Switch,
 } from "react-router-dom";
 import home from "../../Pages/home";
-//import MapPage from "../../pages/MapPage";
+import Share from "../../components/Share/Share";
 import Nav from "../../components/Navbar/Navbar";
 //le routing ne sert a rien tant quil nya pas une persistance pour tt les elements ( post )
 const routes = () => {
@@ -15,7 +15,7 @@ const routes = () => {
       <Nav />
       <Switch>
         <Route path="/" exact component={home} />
-
+        <Route path="/share" exact component={Share} />
         <Redirect to="/" />
       </Switch>
     </Router>
