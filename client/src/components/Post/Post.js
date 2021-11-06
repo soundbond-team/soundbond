daba7e55db361dad0bef0c3ed61b00759e7e667e
Maguitadiop <diopma221@gmail.com>
2021-11-06 11:14:46 +0100

    correction test d'interface + share

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { withRouter } from "react-router-dom";
+import { withRouter,useHistory } from "react-router-dom";
 import AudioPlayer from "../AudioPlayer/AudioPlayer";
 import Avatar from "@material-ui/core/Avatar";
 import Card from "@material-ui/core/Card";
@@ -7,9 +7,9 @@ import { useDispatch } from "react-redux";
 import "../Share/Share";
 import CommentIcon from "@material-ui/icons/Comment";
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
-import { blue } from "@material-ui/core/colors";
+import { red, blue } from "@material-ui/core/colors";
 import Grid from "@material-ui/core/Grid";
-
+import ShareIcon from "@material-ui/icons/Share";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
@@ -60,6 +60,10 @@ function Post(props) {
     dispatch(addLike({ id: props.id_post, like: like + 1 }));
   };
 
+  const history = useHistory();
+
+ 
+
   return (
     <>
       <Card className={classes.card}>
@@ -76,13 +80,14 @@ function Post(props) {
         </Grid>
 
         {
-          <AudioPlayer
+          <AudioPlayer id="son"
             file={null}
             id_son={props.id_position_son}
             latitude={props.latitude}
             longitude={props.longitude}
           />
         }
+       
 
         <Grid item container justifyContent="flex-end">
           <span>
@@ -95,6 +100,15 @@ function Post(props) {
           </span>
 
           <CommentIcon className={classes.icon} style={{ margin: "4px" }} />
+          
+          <span>
+            <ShareIcon
+             onclick={history.push('/share')}
+             style={{ color: red[500] }} 
+            className={classes.icon} 
+            />
+          </span>
+        
         </Grid>
       </Card>
     </>
