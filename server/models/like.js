0d7d5f77e34abd8129c4e3a6db9b005d196a4f71
gu1lhem <38001093@parisnanterre.fr>
2021-11-07 14:29:42 +0100

    Removeing useless code

--- server/models/like.js
+++ server/models/like.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    var like = sequelize.define(sequelize.define('like'), {
+    return sequelize.define(sequelize.define('like'), {
         postId:{
             type:DataTypes.INTEGER,
             references: {
@@ -20,28 +20,4 @@ module.exports = (sequelize, DataTypes) => {
         }
     }, {});
 
-    like.associate = function(models){
-        models.user.belongsToMany(models.post, {
-            through: model.like,
-            foreignKey: userId,
-            otherKey: postId,
-        });
-        models.user.belongsToMany(models.user, {
-            through: model.like,
-            foreignKey: postId,
-            otherKey: userId,
-        });
-
-        models.like.belongsTo(models.user, {
-            foreign: 'userId',
-            as: 'user',
-        });
-
-        models.belongsTo(models.post, {
-            foreignKey: 'postId',
-            as: 'post',
-        });
-    };
-
-    return like;
 }
