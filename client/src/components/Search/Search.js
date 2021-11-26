c704b36be975cf1d97d4100c5bfe2545caaed2d3
Mohamed Belhaimeur <belhaim789@gmail.com>
2021-11-26 01:27:51 +0100

    suggestion user

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
-
+import { ReactSearchAutocomplete } from "react-search-autocomplete";
 import { Tooltip } from "@material-ui/core";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Autocomplete from "@mui/material/Autocomplete";
+import "./Search.css";
 function Search() {
   const [recherche, setRecherche] = useState(" ");
   const refinput = React.useRef();
@@ -10,6 +14,8 @@ function Search() {
   const [userexist, setUserexist] = useState(false);
   const [open, setOpen] = React.useState(false);
   const navigate = useNavigate();
+
+  const [userSuggestion, setUserSuggestion] = useState([]);
   const navigateToTag = async () => {
     await findIfTagExist(recherche);
 
@@ -20,6 +26,7 @@ function Search() {
       setRecherche(" ");
       refinput.current.value = null;
       setOpen(false);
+      setUserSuggestion([]);
       navigate(`/tag/${temptag.substring(1)}`);
     }
   };
@@ -91,43 +98,118 @@ function Search() {
       setRecherche(" ");
       refinput.current.value = null;
       setOpen(false);
+      setUserSuggestion([]);
       navigate(`/profil/${tempuser}`);
     }
   };
+  function onChangesearch(e) {
+    setOpen(false);
+    setRecherche(e.target.value.replace(/\s/g, ""));
+    getAllUsers();
+    console.log(userSuggestion);
+  }
+
+  async function getAllUsers() {
+    await axios({
+      method: "get",
+      url: `http://localhost:8080/api/v1/user/recherche/${recherche}`,
+    })
+      .then((res) => {
+        console.log(res);
+        if (res.data !== "" && res.data != null) {
+          setUserSuggestion(res.data);
+        } else {
+          setUserSuggestion([]);
+        }
+      })
+      .catch((err) => {
+        setUserSuggestion([]);
+      });
+  }
+
+  function navigateToUserOption(name) {
+    setUserexist(false);
+    let tempuser = recherche;
+
+    setRecherche(" ");
+    refinput.current.value = null;
+    setUserSuggestion([]);
+    setOpen(false);
+    navigate(`/profil/${name}`);
+  }
+
+  function getAllTags() {}
   return (
     <>
       <div className="d-flex ">
-        <Tooltip
-          PopperProps={{
-            disablePortal: true,
-          }}
-          onClose={handleTooltipClose}
-          open={open}
-          disableFocusListener
-          disableHoverListener
-          disableTouchListener
-          title={
-            recherche.includes("#")
-              ? "Aucun post ne possède ce tag"
-              : "Aucun utilisateur trouvé"
-          }
-        >
-          <input
-            className="form-control me-2"
-            type="search"
-            placeholder="Rechercher"
-            aria-label="Search"
-            multiple
-            pattern="^\S+$"
-            ref={refinput}
-            value={recherche !== " " ? recherche : null}
-            onChange={(e) => {
-              setRecherche(e.target.value.replace(/\s/g, ""));
+        <>
+          <div
+            style={{
+              width: "200px",
             }}
-            data-toggle="popover"
-            data-content="And here's some amazing content. It's very engaging. Right?"
-          />
-        </Tooltip>
+          >
+            <Tooltip
+              PopperProps={{
+                disablePortal: true,
+              }}
+              onClose={handleTooltipClose}
+              open={open}
+              disableFocusListener
+              disableHoverListener
+              disableTouchListener
+              title={
+                recherche.includes("#")
+                  ? "Aucun post ne possède ce tag"
+                  : "Aucun utilisateur trouvé"
+              }
+            >
+              <input
+                className="form-control me-2"
+                type="search"
+                autocomplete
+                list="suggestion"
+                placeholder="Rechercher"
+                aria-label="Search"
+                multiple
+                pattern="^\S+$"
+                ref={refinput}
+                value={recherche !== " " ? recherche : null}
+                onChange={onChangesearch}
+                data-toggle="popover"
+                data-content="And here's some amazing content. It's very engaging. Right?"
+              />
+            </Tooltip>
+            <ul
+              style={{
+                listStyle: "none",
+
+                width: "200px",
+                position: "fixed",
+                zIndex: "10",
+              }}
+            >
+              {userSuggestion.length > 0 ? (
+                userSuggestion.map((user) => (
+                  <li
+                    onClick={() => navigateToUserOption(user.username)}
+                    style={{
+                      padding: "5px 10px 5px 10px",
+                      border: "1px solid black",
+                      cursor: "pointer",
+                      backgroundColor: "white",
+                    }}
+                    key={user.id}
+                  >
+                    {user.username}
+                  </li>
+                ))
+              ) : (
+                <span />
+              )}
+            </ul>
+          </div>
+        </>
+
         <div className="input-group-append">
           {" "}
           <div>
