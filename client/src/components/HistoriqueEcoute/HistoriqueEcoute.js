82bcefd18f653c97a6b7615b473c69547bbb8d34
Maguitadiop <diopma221@gmail.com>
2022-03-13 17:27:22 +0100

    refactoring

--- client/src/components/HistoriqueEcoute/HistoriqueEcoute.js
+++ client/src/components/HistoriqueEcoute/HistoriqueEcoute.js
@@ -1,15 +1,10 @@
 import React, { useEffect } from "react";
-import Post from "../../components/Post/Post";
-
-import Grid from "@material-ui/core/Grid";
-
+import Playlist from "../../components/MyPlayLists/MyPlaylists";
 import { useDispatch, useSelector } from "react-redux";
-
 import { getPostVisitedByUser } from "../../actions/post.actions";
 
 function HistoriqueEcoute(){
-  const allplaylistByUser = useSelector((state) => state.allplaylistByUser);
-
+   
   const currentUserdata = useSelector((state) => state.getotherprofiluser);
   const dispatch = useDispatch();
 
@@ -21,34 +16,7 @@ function HistoriqueEcoute(){
 
   return (
     <>
-      {currentUserdata ? (
-        <>
-          <br />
-          {
-            <Grid container direction="column-reverse" spacing={3}>
-              {allplaylistByUser.length > 0 ? (
-                allplaylistByUser.map((i, index) => (
-                  <Grid key={index} item>
-                    <Post post={i} />
-                  </Grid>
-                ))
-              ) : (
-                <Grid item>
-                  <br />
-                  <div className="container ">
-                    {" "}
-                    <p className="d-flex  justify-content-center">
-                      Aucun post pour le moment
-                    </p>
-                  </div>
-                </Grid>
-              )}
-            </Grid>
-          }
-        </>
-      ) : (
-        <p></p>
-      )}
+      <Playlist />
     </>
   );
 }
