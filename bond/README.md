bbcd8b85d3e423bca7777b99ef42a0d4ca06788a
malinvaudpaul <paulmalinvaud@proton.me>
2023-01-14 09:57:15 +0100

    Initialisation des credentials

--- bond/README.md
+++ bond/README.md
@@ -3,3 +3,10 @@
 
 Ici on se focalise sur l'aspect réseau social de SoundBond. L'idée est de travailler à la suggestion d'utilisateur à suivre pour un utilisateur du réseau.
 
+## Configuration des variables d'environnement
+
+Pour éviter de mettre nos credentials de l'API Twitter, nous utilisons un fichier .env qui contient les variables d'environnement. Ce fichier est ignoré par git. Pour l'avoir en local, exécuter, dans le fichier /scripts:
+```sh 
+cp .env.example .env
+```
+Ensuite, remplir les variables d'environnement avec nos credentials. (ask Paul)
--- bond/scripts/twitter-scraper.py
+++ bond/scripts/twitter-scraper.py
@@ -1,20 +1,22 @@
 import tweepy
 import pandas as pd
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-#Clés et jetons
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
+# Clés et jetons
+consumer_key = os.getenv("CONSUMER_KEY")
+consumer_secret = os.getenv("CONSUMER_SECRET")
+access_token = os.getenv("ACCESS_TOKEN")
+access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
-#paramètres pour initialiser l’API
+# paramètres pour initialiser l’API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
+api = tweepy.API(auth)
 
-
-#liste des abonnés de l'utilisateur(1210627806).
+# liste des abonnés de l'utilisateur(1210627806).
 user_list = ["1210627806"]
 follower_list = []
 for user in user_list:
@@ -28,12 +30,12 @@ for user in user_list:
         continue
     follower_list.append(followers)
 
-df = pd.DataFrame(columns=['source','target']) # DataFrame
-df['target'] = follower_list[0] # Colonne cible
-df['source'] = 1210627806 # Id utilisateur en tant que source
+df = pd.DataFrame(columns=["source", "target"])  # DataFrame
+df["target"] = follower_list[0]  # Colonne cible
+df["source"] = 1210627806  # Id utilisateur en tant que source
 
-#On utilise la liste des abonnés déja extraite dans le code ci-dessus(450 abonnés)
-user_list2 = list(df['target'])
+# On utilise la liste des abonnés déja extraite dans le code ci-dessus(450 abonnés)
+user_list2 = list(df["target"])
 for userID in user_list2:
     print(userID)
     followers2 = []
@@ -49,16 +51,14 @@ for userID in user_list2:
         for page in tweepy.Cursor(api.followers_ids, user_id=userID).pages():
             followers2.extend(page)
             print(len(followers2))
-            if followers_count >= 5000: # On prend que les 5000 premiers abonnés
+            if followers_count >= 5000:  # On prend que les 5000 premiers abonnés
                 break
     except tweepy.TweepError:
         print("error")
         continue
     follower_list2.append(followers2)
-    temp = pd.DataFrame(columns=['source', 'target'])
-    temp['target'] = follower_list2[0]
-    temp['source'] = userID
+    temp = pd.DataFrame(columns=["source", "target"])
+    temp["target"] = follower_list2[0]
+    temp["source"] = userID
     df = df.append(temp)
     df.to_csv("networkOfFollowers.csv")
-
-    
