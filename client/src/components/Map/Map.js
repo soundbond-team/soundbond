dc5deb4cc1248327f229bd4776f5aef768137abd
Mohamed Belhaimeur <belhaim789@gmail.com>
2023-01-19 14:58:43 +0100

    heatmap

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -148,6 +148,7 @@ const Map = ({ post_points, connected_user_id }) => {
           return null;
         }
       });
+
       if (itineraire.length > 0) {
         const query = await fetch(
           `https://api.mapbox.com/directions/v5/mapbox/driving/${start[0]},${start[1]}${line}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
@@ -185,74 +186,85 @@ const Map = ({ post_points, connected_user_id }) => {
             "line-opacity": 0.75,
           },
         });
-
-        map.addLayer(
-          {
-            id: 'trees-heat',
-            type: 'heatmap',
-            source: {
-              type: "geojson",
-              data: geojson,
-            },
-            maxzoom: 15,
-            paint: {
-              // increase weight as diameter breast height increases
-              'heatmap-weight': {
-                property: 'dbh',
-                type: 'exponential',
-                stops: [
-                  [1, 0],
-                  [62, 1]
-                ]
-              },
-              // increase intensity as zoom level increases
-              'heatmap-intensity': {
-                stops: [
-                  [11, 1],
-                  [15, 3]
-                ]
-              },
-              // assign color values be applied to points depending on their density
-              'heatmap-color': [
-                'interpolate',
-                ['linear'],
-                ['heatmap-density'],
-                0,
-                'rgba(236,222,239,0)',
-                0.2,
-                'rgb(208,209,230)',
-                0.4,
-                'rgb(166,189,219)',
-                0.6,
-                'rgb(103,169,207)',
-                0.8,
-                'rgb(28,144,153)'
-              ],
-              // increase radius as zoom increases
-              'heatmap-radius': {
-                stops: [
-                  [11, 15],
-                  [15, 20]
-                ]
-              },
-              // decrease opacity to transition into the circle layer
-              'heatmap-opacity': {
-                default: 1,
-                stops: [
-                  [14, 1],
-                  [15, 0]
-                ]
-              }
-            }
-          },
-          'waterway-label'
-        );
       }
     }
-
-    
-
+    console.log(post_points);
     map.on("load", () => {
+      //add a layer heatmap for markers post_points
+      map.addLayer({
+        id: "points",
+        type: "heatmap",
+        source: {
+          type: "geojson",
+          data: {
+            type: "FeatureCollection",
+            features: post_points.map((point) => ({
+              type: "Feature",
+              geometry: {
+                type: "Point",
+                coordinates: [
+                  point.publishing.soundlocation.longitude,
+                  point.publishing.soundlocation.latitude,
+                ],
+              },
+              properties: {
+                description: point.description,
+                publisher: point.publisher.username,
+              },
+            })),
+          },
+        },
+        paint: {
+          // increase weight as diameter breast height increases
+          "heatmap-weight": {
+            property: "dbh",
+            type: "exponential",
+            stops: [
+              [1, 0],
+              [62, 1],
+            ],
+          },
+          // increase intensity as zoom level increases
+          "heatmap-intensity": {
+            stops: [
+              [11, 1],
+              [15, 3],
+            ],
+          },
+          // assign color values be applied to points depending on their density
+          "heatmap-color": [
+            "interpolate",
+            ["linear"],
+            ["heatmap-density"],
+            0,
+            "rgba(236,222,239,0)",
+            0.2,
+            "rgb(208,209,230)",
+            0.4,
+            "rgb(166,189,219)",
+            0.6,
+            "rgb(103,169,207)",
+            0.8,
+            "rgb(28,144,153)",
+          ],
+          // increase radius as zoom increases
+          "heatmap-radius": {
+            stops: [
+              [11, 15],
+              [15, 20],
+            ],
+          },
+          // decrease opacity to transition into the circle layer
+          "heatmap-opacity": {
+            default: 1,
+            stops: [
+              [14, 1],
+              [15, 0],
+            ],
+          },
+        },
+      });
+
       setMap(map); // We declare the map as a State to make it available for every functions.
       addMarkers(map, post_points);
       getRoute();
