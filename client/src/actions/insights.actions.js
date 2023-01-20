19c02d6fa8dc865c5238b4151b43ef8f24a28c12
RizlaneAbalil <38002332@parisnanterre.fr>
2023-01-20 10:23:51 +0100

    add new stats

--- client/src/actions/insights.actions.js
+++ client/src/actions/insights.actions.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const GET_MOST_LISTENED = "GET_MOST_LISTENED";
+export const GET_TOP_TREND = "GET_TOP_TREND";
 
 
 // Récupérer tous les users qui sont les plus écoutés 
@@ -16,4 +17,30 @@ export const most_listened_users = (user_id) => {
         .catch((err) => console.log(err));
     };
   };
+
+export const top_trend = () => {
+    return (dispatch) => {
+      return axios({
+        method: "get",
+        url: process.env.REACT_APP_BACK_SERVER_URL+`api/v1/user/stats/tags`,
+      })
+        .then((res) => {
+          dispatch({ type: GET_TOP_TREND, payload: res.data });
+        })
+        .catch((err) => console.log(err));
+    };
+  };
+
+export const number_posts_during_period = () => {
+    return (dispatch) => {
+      return axios({
+        method: "get",
+        url: process.env.REACT_APP_BACK_SERVER_URL+`api/v1/user/stats/tags`,
+      })
+        .then((res) => {
+          dispatch({ type: GET_TOP_TREND, payload: res.data });
+        })
+        .catch((err) => console.log(err));
+    };
+};
   
